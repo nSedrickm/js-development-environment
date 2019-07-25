2cf52754e8f035baa97690803cf672d0df76764d
nSedrickm <nsedrick101@gmail.com>
2019-07-25 18:54:58 +0100

    installed babel-cli global

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,13 +8,13 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.usee(require('webpack-dev-middleware')(compiler, {
+app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
 app.get ('/' ,function (req,res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
