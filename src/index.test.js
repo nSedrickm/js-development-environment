14d7b18269997baa037536a09eaacda1af0e90b8
nSedrickm <nsedrick101@gmail.com>
2019-07-26 20:11:16 +0100

    Second test

--- src/index.test.js
+++ src/index.test.js
@@ -5,3 +5,16 @@ describe('Our first test', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('index.html', () => {
+  it('should say hello', () => {
+    let fs = require('fs');
+    let jsdom = require('jsdom');
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.querySelector('h1');
+      expect(h1.innerHTML).to.equal("Hello World!");
+      window.close();
+    });
+  })
+})
