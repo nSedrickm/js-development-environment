b268981e421ce035f346a0de91028a5e16301303
nsedrickm <nsedrick101@gmail.com>
2019-11-03 09:22:52 -0500

    module 9 travis CI start

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,6 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
@@ -7,13 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', () => {
-    let fs = require('fs');
-    let jsdom = require('jsdom');
+  it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
-      const h1 = window.document.querySelector('h1');
+      const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("Hello World!");
+      done();
       window.close();
     });
   })
