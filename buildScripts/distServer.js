b9bdbedc466326c85d629434da59bd08fac05959
nsedrickm <nsedrick101@gmail.com>
2019-11-04 11:16:30 +0100

    deploying to heroku and surge

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,16 +15,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function (req, res) {
-  //simulating a real db
-  res.json([
-    { "id": 1, "firstName": "Bob", "lastName": "Lesly", "email": "bob@gmail.com" },
-
-    { "id": 2, "firstName": "Tammy", "lastName": "Smith", "email": "tammy@gmail.com" },
-
-    { "id": 3, "firstName": "Falj", "lastName": "Garand", "email": "falj@gmail.com" }
-  ]);
-});
 
 app.listen(port, function (err) {
   if (err) {
